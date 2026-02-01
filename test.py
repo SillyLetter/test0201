8e9f4843d202ce6cecbb9f12acef6f70aae9e910
SillyLetter <110168381+SillyLetter@users.noreply.github.com>
2026-02-02 03:54:44 +0800

    設定正式環境資料庫

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
-a = 5
-b = 3
-c = a+b
-print(c)
+import os
 
-##456456456456456456
+def get_database_url():
+    """
+    獲取資料庫連線網址。
+    在解衝突作業中，不同分支將會對這行有不同的期待。
+    """
+    
+    DATABASE_URL = "https://prod.database.com" 
+    
+    return DATABASE_URL
+
+if __name__ == "__main__":
+    url = get_database_url()
+    print(f"✅ 系統啟動中... 連線至：{url}")
